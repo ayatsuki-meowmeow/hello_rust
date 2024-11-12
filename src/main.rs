ed2a962b588475168dfcf6a06c7d065eb158cf93
kono-meowmeow <aya.ayatsuki@gmail.com>
2024-11-12 22:24:12 +0900

    feat: Update main.rs to use ferris_says crate for printing a message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say; // ferris_saysのsay関数をimportする
+use std::io::{stdout, BufWriter};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 }
